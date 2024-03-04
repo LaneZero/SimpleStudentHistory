67abb80ee9ee361c7a23fa4603a4a958037258af
LaneZero <Ahmad.Rahimi@live.com>
2024-03-05 01:36:20 +0330

    create menu

--- StudentHistory/Program.cs
+++ StudentHistory/Program.cs
@@ -1,1 +1,23 @@
 ﻿
+
+using System.ComponentModel.Design;
+
+List<string>[][] studentName = [];
+
+menu();
+
+void menu()
+{
+    Console.WriteLine("*****************");
+    Console.WriteLine("Welcome to Students History v1.0");
+    Console.WriteLine("*****************");
+    Console.WriteLine();
+    Console.WriteLine("Choose your operation Number:");
+    Console.WriteLine();
+    Console.WriteLine("1.Add Students and Number");
+    Console.WriteLine("2.Show All Students");
+    Console.WriteLine("3.Show All Students Avarage");
+    Console.WriteLine("4.Show Student Avarage by ID");
+    Console.WriteLine("5.[E]xit by ID");
+    Console.WriteLine();
+}
